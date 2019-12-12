8ace4191ff2700884a2bbb00281fdf84924053c9
Pritam Patil <pritam.patil@optile.net>
2019-12-12 12:09:55 +0100

    home page tests and gitignore file

---
+++ cypress/integration/homepageTest.spec.js
@@ -0,0 +1,14 @@
+describe('Checks Opencodez Home Page', ()=>{
+    it('Opens the page', ()=>{
+        cy.visit('https://www.opencodez.com')
+    });
+
+    it('Verifies the logo', () => {
+        cy.get('#logo').should('be.visible')
+    })
+
+    it('Checks the search functionality', () => {
+        cy.get('#search-4').type('NodeJS');
+        cy.get('#search-image').click();
+    })
+})
